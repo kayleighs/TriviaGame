8a47badca705cab684e2c3e0e187253f6e9b2daa
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-05-21 13:53:04 -0400

    message boxes hiding and showing correctly, results append on different div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,6 +26,8 @@ $("#start").on("click", function(){
 //on click for start over
 $(document).ready(function () {
     $("#start-over").hide();
+    $("#time-box").hide();
+    $("#message-box").hide();
 });
 $("#start-over").on("click", function () {
     $("#start-over").hide();
@@ -33,6 +35,8 @@ $("#start-over").on("click", function () {
 });
 //function for new game
 function newGame() {
+    $("#time-box").show(); 
+    $("#message-box").show();
     currentQuestion=0;
     correct=0;
     incorrect=0;
@@ -44,22 +48,25 @@ function newGame() {
 function newQuestion() {
     $(".choices").empty();
     $("#gif").empty();
- //   $("#message").empty();
+    $("#message").empty();
+    $("#time-box").show();
 
     $("#currentQuestionNum").html("Question #"+(currentQuestion +1) + " out of " + questions.length);
     $(".question").html(questions[currentQuestion].question)
      for (var i = 0; i < questions[currentQuestion].answerChoice.length ; i+=1) {
-        var choices = $("<div>");
+        var choices = $("<button>");
+        var lineBreak= $("<br>");
         choices.text(questions[currentQuestion].answerChoice[i]);
         choices.attr({ "data-index": i });
-        choices.addClass("userChoice");
+        choices.addClass("userChoice ks-buttonChoices");
         $(".choices").append(choices); 
+        $(".choices").append(lineBreak);
+
 } 
     countdown();
 
     $(".userChoice").on('click', function () {
         userSelect = $(this).data("index")
-  //      userSelect = $(this)
         console.log(this);
         clearInterval(counter)
         answerStage();
@@ -68,7 +75,7 @@ function newQuestion() {
 
 //function to countdown at each question
 function countdown() {
-    seconds = 15; 0
+    seconds = 15; 
     $('#timeLeft').html("Time Remaining: " + seconds);
     counter = setInterval(showCountdown, 1000)
 }
@@ -82,11 +89,43 @@ function showCountdown() {
          answerStage();
     }
 }
+
+function countdownAnswer() {
+    seconds = 1;
+    $('#timeLeft').html("Time Remaining: " + seconds);
+    counter = setInterval(showCountdownAnswer, 1000)
+}
+function showCountdownAnswer() {
+    seconds -= 1;
+    $('#timeLeft').html("Time Remaining: " + seconds);
+    if (seconds <= 0) {
+        clearInterval(counter);
+        scorePage ();
+        //      return
+        //     answered = false;
+    }
+}
+function countdownAnswerNext() {
+    seconds = 1;
+    $('#timeLeft').html("Time Remaining: " + seconds);
+    counter = setInterval(showCountdownAnswerNext, 1000)
+}
+function showCountdownAnswerNext() {
+    seconds -= 1;
+    $('#timeLeft').html("Time Remaining: " + seconds);
+    if (seconds <= 0) {
+        clearInterval(counter);
+        newQuestion();
+        //      return
+        //     answered = false;
+    }
+}
 function answerStage() {
     $ (".question").empty();
     $(".choices").empty();
-    $("#timeLeft").empty();
     $("#gif").html('<img src = "assets/images/' + gifs[currentQuestion] + '.gif" width = "400px">');  //" and ' are important here
+
+    $('#timeLeft').html("Time Remaining: " + seconds);
     var rightAnswerText= questions[currentQuestion].answerChoice[questions[currentQuestion].answer]
     var rightAnswerIndex= questions[currentQuestion].answer
     
@@ -96,7 +135,7 @@ function answerStage() {
         correct +=1;
         $("#message").text("Correct!")
     }
-      else if (userSelect != rightAnswerIndex) {
+    else if (userSelect != rightAnswerIndex) {
         incorrect +=1;
         $("#message").html("You are wrong! ")
         $("#message").append("The correct answer is: "+ rightAnswerText)
@@ -107,22 +146,45 @@ function answerStage() {
         $("#message").append("The correct answer is: " + rightAnswerText)
     }
     if (currentQuestion=== (questions.length-1)) {
-        setTimeout(scorePage, 1000)
+        countdownAnswer();
     }
     else {
         currentQuestion +=1;
-        setTimeout(newQuestion, 5000)
+        countdownAnswerNext();
     }
 }
+
 function scorePage () {
     $("#currentQuestionNum").empty();
+    $("#time-box").hide();
+    $("#timeLeft").empty();
     $("#gif").empty();
-    $("#message").html("All Done!")
-    $("#message").append("Correct Answers: " + correct)
-    $("#message").append("Incorrect Answers: " + incorrect)
-    $("#message").append("Unanswered Answers: " + unanswered)
+    $("#message").empty();
+    $("#start-over").show();
+
+
+    var doneh = $("<h3>")
+    doneh.text("All Done!")
+
+    var corDiv = $("<div>");
+    corDiv.text("Correct Answers: " + correct)
+
+    var incorDiv = $("<div>");
+    incorDiv.text("Incorrect Answers: " + incorrect)
+  
+    var unansweredDiv = $("<div>");
+    unansweredDiv.text("Unanswered Answers: " + unanswered)
+
+    $("#message").append(doneh)
+    $("#message").append(corDiv)
+    $("#message").append(incorDiv)
+    $("#message").append(unansweredDiv)
+
+ 
     var percentage = Math.floor((correct /questions.length) * 100);
     console.log(percentage)
-    $("#message").append("Percentage: " + percentage + "%")
+
+-    $("#message").append("Percentage: " + percentage + "%")
 
 }
+
